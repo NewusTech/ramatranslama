29fd1ca59caef22588ac9ed6cb9bdda88ebf806b
Gilang Rizky Ramadhan <gilang.119140081@student.itera.ac.id>
2024-08-20 16:39:34 +0700

    update warna

--- resources/views/frontend/beranda/index.blade.php
+++ resources/views/frontend/beranda/index.blade.php
@@ -145,8 +145,17 @@
                                                         <i class="fa fa-times" id="clearJam"></i>
                                                     </div>
                                                 </div>
-                                                <button type="submit" class="button-search"
-                                                    style="border: 0">Cari</button>
+                                                <button type="submit" class="btn
+    @if (env('APP_NAME') == 'Rama Tranz Lampung')
+        btn-warning
+    @else
+        btn-primary
+    @endif
+" style="border: 0; width: 10%;">Cari</button>
+
+
+
+
                                             </div>
                                         </form>
                                     </div>
@@ -204,10 +213,17 @@
                                                     </div>
 
                                                     <div class="ct-reserve">
-                                                        <a class="btn btn-primary details-button text-white"
+                                                        <a class="btn 
+                                                            @if (env('APP_NAME') == 'Rama Tranz Lampung')
+                                                                btn-warning
+                                                            @else
+                                                                btn-primary
+                                                            @endif
+                                                            details-button text-white"
                                                             data-toggle="modal" data-item="{{ $layanans }}"
                                                             data-target="#modalBookingIndex">Pesan</a>
                                                     </div>
+
                                                 </div>
                                             </div>
                                             <div class="acr-bg">
@@ -340,7 +356,14 @@
                                 </div>
                                 <div class="features-info-bot">
                                     <h4 class="title"><span class="font-4">Lokasi</span> {{ $outlet->nama_provinsi }}
-                                    </h4><a href="{{ route('locationId', $outlet->slug) }}" class="button">Details</a>
+                                    </h4><a href="{{ route('locationId', $outlet->slug) }}" class="button 
+    @if (env('APP_NAME') == 'Rama Tranz Lampung')
+        btn-warning
+    @else
+        btn-primary
+    @endif
+">Details</a>
+
                                 </div>
                             </div>
                         </div>
@@ -431,10 +454,19 @@
                                                     <h6 class="blog-title">{{ $layanan->title }}</h6>
                                                 </a>
                                             </div>
-                                            <a target="_blank"
-                                                href="https://api.whatsapp.com/send?phone={{ $hq && substr($hq->phone_1, 0, 2) == '08' ? '62' . substr($hq->phone_1, 1) : $hq->phone_1 }}&text=Hallo%2C%20Saya%20ingin%20memesan%20tiket%20perjalanan%20di%20Rama%20Trans%20Travel.%20Untuk%20pemesanannya%20bagaimana%20ya%3F"
-                                                class="cws-button box-bottom small alt" target="_blank">Pesan
-                                            </a>
+                                            <a 
+    target="_blank"
+    href="https://api.whatsapp.com/send?phone={{ $hq && substr($hq->phone_1, 0, 2) == '08' ? '62' . substr($hq->phone_1, 1) : $hq->phone_1 }}&text=Hallo%2C%20Saya%20ingin%20memesan%20tiket%20perjalanan%20di%20Rama%20Trans%20Travel.%20Untuk%20pemesanannya%20bagaimana%20ya%3F"
+    class="cws-button box-bottom small 
+        @if (env('APP_NAME') == 'Rama Tranz Lampung')
+            btn-warning
+        @else
+            btn-primary
+        @endif
+        alt" 
+    target="_blank">Pesan
+</a>
+
                                         </div>
                                     </div>
                                 </div>
