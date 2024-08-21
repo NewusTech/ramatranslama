fead97c0c958029361ae9db6292a06380d0a9c4c
Gilang Rizky Ramadhan <gilang.119140081@student.itera.ac.id>
2024-08-21 14:17:28 +0700

    update color

--- resources/views/frontend/beranda/index.blade.php
+++ resources/views/frontend/beranda/index.blade.php
@@ -360,15 +360,15 @@
                                 </div>
                                 <div class="features-info-bot">
                                     <h4 class="title"><span class="font-4">Lokasi</span> {{ $outlet->nama_provinsi }}
-                                    </h4><a href="{{ route('locationId', $outlet->slug) }}" class="button 
-    @if (env('APP_NAME') == 'Rama Tranz Lampung')
-        btn-primary
-    @else
-        
-        btn-warning
-    @endif
-">Details</a>
-
+                                    </h4>
+                                    <a href="{{ route('locationId', $outlet->slug) }}" class="button 
+                                        @if (env('APP_NAME') == 'Rama Tranz Lampung')
+                                            btn-primary
+                                        @else
+                                            
+                                            btn-warning
+                                        @endif">Details
+                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -465,19 +465,19 @@
                                                 </a>
                                             </div>
                                             <a 
-    target="_blank"
-    href="https://api.whatsapp.com/send?phone={{ $hq && substr($hq->phone_1, 0, 2) == '08' ? '62' . substr($hq->phone_1, 1) : $hq->phone_1 }}&text=Hallo%2C%20Saya%20ingin%20memesan%20tiket%20perjalanan%20di%20Rama%20Trans%20Travel.%20Untuk%20pemesanannya%20bagaimana%20ya%3F"
-    class="cws-button box-bottom small 
-        @if (env('APP_NAME') == 'Rama Tranz Lampung')
-            btn-primary
-        @else
-            
-            btn-warning
-        @endif
-        alt" 
-    target="_blank">Pesan
-</a>
-
+                                                target="_blank"
+                                                href="https://api.whatsapp.com/send?phone={{ $hq && substr($hq->phone_1, 0, 2) == '08' ? '62' . substr($hq->phone_1, 1) : $hq->phone_1 }}&text=Hallo%2C%20Saya%20ingin%20memesan%20tiket%20perjalanan%20di%20Rama%20Trans%20Travel.%20Untuk%20pemesanannya%20bagaimana%20ya%3F"
+                                                class="cws-button
+                                                 box-bottom small 
+                                                    @if (env('APP_NAME') == 'Rama Tranz Lampung')
+                                                        btn-primary
+                                                    @else
+                                                        
+                                                        btn-warning
+                                                    @endif
+                                                    alt" 
+                                                target="_blank">Pesan
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -601,7 +601,15 @@
         <!-- ! gallery section-->
 
         <!-- call out section-->
-        <section class="page-section cws_prlx_section bg-white-80 pb-60 pt-60" style="background-color: #1bb583">
+        <section class="page-section cws_prlx_section bg-white-80 pb-60 pt-60" 
+    style="
+        background-color: 
+        @if (env('APP_NAME') == 'Rama Tranz Lampung')
+            
+            #007bffcc; /* Warna kuning pudar */
+        @else
+            #ffc107cc; /* Warna biru pudar */
+        @endif">
             <div class="container">
                 <div class="call-out-box">
                     <div class="call-out-wrap alt">
